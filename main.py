310d399c011aae78d457a990a15f6286811af300
Soufiane-Majdar <soufianemj3145@gmail.com>
2023-07-02 22:29:04 +0200

    new update

--- main.py
+++ main.py
@@ -8,9 +8,59 @@ path = 'persons'
 images = []
 classNames = []
 personList = os.listdir(path)
-print(personList)
+# print(personList)
 
 for person in personList:
     curImg = cv2.imread(f'{path}/{person}')
     images.append(curImg)
     classNames.append(os.path.splitext(person)[0])
+print(classNames)
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+encodeListKnown = findEncodings(images)
+# print(encodeListKnown)
+print('Encoding Complete')
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, img = cap.read()
+
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS,cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
+
+    for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            print(name)
+            y1,x2,y2,x1 = faceLoc
+            # multiply by 4 to get the original size of the image
+            y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+
+    cv2.imshow('Webcam',img)
+    cv2.waitKey(1)
+
+
+
+
+
+        
+
